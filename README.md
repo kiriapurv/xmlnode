88f26a08ae7cdb1c3c17f9133a6680b176dc76b4
Apurv Kiri <i@apurv.me>
2013-12-24 18:14:14 +0530

    Initial version

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
 XMLNode
 =======
 
-Simple XML creator
+Simple XML creator for NodeJS
+
+### Sample
+
+````
+        var XMLNode = require("./xmlnode.js");
+		var parent = new XMLNode("Parent");
+		parent.setAttrs({
+			"key1" : "value1",
+			"key2" : "value2"
+		});
+		
+		var child = new XMLNode("Child");
+		var grandChild = new XMLNode("GrandChild");
+		grandChild.setText("This is plain text");
+		child.addNode(grandChild);
+		parent.addNode(child);
+		
+		child = new XMLNode("Child");
+		grandChild = new XMLNode("GrandChild");
+		grandChild.setCData("This is text wrapped in CDATA");
+		child.addNode(grandChild);
+		parent.addNode(child);
+		
+		console.log(parent.getXml());
+````
+
+### Ouput
+
+````
+<Parent key1="value1" key2="value2">
+    <Child>
+        <GrandChild>This is plain text</GrandChild>
+    </Child>
+    <Child>
+        <GrandChild>
+            <![CDATA[ This is text wrapped in CDATA ]]>
+        </GrandChild>
+    </Child>
+</Parent>
+````
---
+++ xmlnode.js
@@ -0,0 +1,57 @@
+//Initialize XMLNode with its tag name
+var XMLNode = function(tagName) {
+    this.tagName = tagName;
+    this.content = null;
+    this.myNodes = [];
+    this.attrs = {};
+};
+
+//Set XML Node's attributes as 
+//``` { key1:value1, key2:value2 .. } ```
+XMLNode.prototype.setAttrs = function(attrs) {
+    this.attrs = attrs;
+};
+
+//Add child node
+XMLNode.prototype.addNode = function(xmlNode) {
+    this.myNodes[this.myNodes.length] = xmlNode;
+};
+
+//Returns XML String representation of current node + its child nodes
+XMLNode.prototype.getXml = function() {
+    if(this.content) {
+        return "<"+this.tagName+">"+this.content+"</"+this.tagName+">";
+    } else {
+        var start = "<"+this.tagName+(this._getAttrs())+">";
+        var cont = "";
+        var node;
+        for(node in this.myNodes) {
+            cont+=this.myNodes[node].getXml();
+        }
+        var end = "</"+this.tagName+">";
+        return start+cont+end;
+    }
+};
+
+//Set plain text to current node
+XMLNode.prototype.setText = function(content) {
+	this.content = content;
+};
+
+//Set text wrapped by CDATA
+XMLNode.prototype.setCData = function(content) {
+	this.content = "<![CDATA["+content+"]]>";
+};
+
+//Returns string representation of attributes as
+// key1="value1" key2="value2" 
+XMLNode.prototype._getAttrs = function() {
+    var attrs = "";
+    var attr;
+    for(attr in this.attrs ) {
+        attrs+=" "+attr+"=\""+this.attrs[attr]+"\"";
+    }
+    return attrs;
+};
+
+module.exports = XMLNode;
