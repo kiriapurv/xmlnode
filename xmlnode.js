3128cb6b4888bce73526677734c74f91b434e102
Apurv Kiri <i@apurv.me>
2014-01-18 00:46:56 +0530

    Added additional parameters to constructor

--- xmlnode.js
+++ xmlnode.js
@@ -1,55 +1,55 @@
 //Initialize XMLNode with its tag name
-var XMLNode = function(tagName) {
+var XMLNode = function (tagName, content, attrs) {
     this.tagName = tagName;
-    this.content = null;
+    this.content = content;
     this.myNodes = [];
-    this.attrs = {};
+    this.attrs = attrs;
 };
 
 //Set XML Node's attributes as 
 //``` { key1:value1, key2:value2 .. } ```
-XMLNode.prototype.setAttrs = function(attrs) {
+XMLNode.prototype.setAttrs = function (attrs) {
     this.attrs = attrs;
 };
 
 //Add child node
-XMLNode.prototype.addNode = function(xmlNode) {
+XMLNode.prototype.addNode = function (xmlNode) {
     this.myNodes[this.myNodes.length] = xmlNode;
 };
 
 //Returns XML String representation of current node + its child nodes
-XMLNode.prototype.getXml = function() {
-    if(this.content) {
-        return "<"+this.tagName+">"+this.content+"</"+this.tagName+">";
+XMLNode.prototype.getXml = function () {
+    if (this.content) {
+        return "<" + this.tagName + ">" + this.content + "</" + this.tagName + ">";
     } else {
-        var start = "<"+this.tagName+(this._getAttrs())+">";
+        var start = "<" + this.tagName + (this._getAttrs()) + ">";
         var cont = "";
         var node;
-        for(node in this.myNodes) {
-            cont+=this.myNodes[node].getXml();
+        for (node in this.myNodes) {
+            cont += this.myNodes[node].getXml();
         }
-        var end = "</"+this.tagName+">";
-        return start+cont+end;
+        var end = "</" + this.tagName + ">";
+        return start + cont + end;
     }
 };
 
 //Set plain text to current node
-XMLNode.prototype.setText = function(content) {
-	this.content = content;
+XMLNode.prototype.setText = function (content) {
+    this.content = content;
 };
 
 //Set text wrapped by CDATA
-XMLNode.prototype.setCData = function(content) {
-	this.content = "<![CDATA["+content+"]]>";
+XMLNode.prototype.setCData = function (content) {
+    this.content = "<![CDATA[" + content + "]]>";
 };
 
 //Returns string representation of attributes as
 // key1="value1" key2="value2" 
-XMLNode.prototype._getAttrs = function() {
+XMLNode.prototype._getAttrs = function () {
     var attrs = "";
     var attr;
-    for(attr in this.attrs ) {
-        attrs+=" "+attr+"=\""+this.attrs[attr]+"\"";
+    for (attr in this.attrs) {
+        attrs += " " + attr + "=\"" + this.attrs[attr] + "\"";
     }
     return attrs;
 };
